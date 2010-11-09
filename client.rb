4776e4519fd600a0dc109f66778b6ff4903191b8
bosh <ohwaitnvm@gmail.com>
2010-11-09 03:57:38 -0500

    testing websockets on energon

--- client.rb
+++ client.rb
@@ -1,12 +1,45 @@
-require 'socket'      # Sockets are in standard library
+# ###
+# ###			TCP RUBY CLIENT
+# ###
+# require 'socket'      # Sockets are in standard library
 
-hostname = 'localhost'
-port = 23000
+# hostname = 'localhost'
+# port = 23000
 
-s = TCPSocket.open(hostname, port)
+# s = TCPSocket.open(hostname, port)
 
-while line = s.gets   # Read lines from the socket
-  puts line.chop      # And print with platform line terminator
+# while line = s.gets   # Read lines from the socket
+#   puts line.chop      # And print with platform line terminator
+# end
+
+# s.close               # Close the socket when done
+
+###################################################
+###
+###				WEBSOCKETS RUBY CLIENT
+###
+# Copyright: Hiroshi Ichikawa <http://gimite.net/en/>
+# Lincense: New BSD Lincense
+
+$LOAD_PATH << File.dirname(__FILE__) + "/../lib"
+require "web_socket"
+
+if ARGV.size != 1
+  $stderr.puts("Usage: ruby samples/stdio_client.rb ws://HOST:PORT/")
+  exit(1)
 end
 
-s.close               # Close the socket when done
+client = WebSocket.new(ARGV[0])
+puts("Connected")
+Thread.new() do
+  while data = client.receive()
+    printf("Received: %p\n", data)
+  end
+  exit()
+end
+$stdin.each_line() do |line|
+  data = line.chomp()
+  client.send(data)
+  printf("Sent: %p\n", data)
+end
+client.close()
