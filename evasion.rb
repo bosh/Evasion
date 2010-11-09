b4abcfb3b1932a85b4e597002b167c9982a5ff2e
bosh <ohwaitnvm@gmail.com>
2010-11-09 04:20:13 -0500

    derpin hard

--- evasion.rb
+++ evasion.rb
@@ -1,28 +1,11 @@
-#!/usr/bin/env ruby
-require 'rubygems'
-require 'sinatra'
 require 'socket'               # Get sockets from stdlib
+server = TCPServer.open(20000)  # Socket to listen on port 2000
 
+clients = []
+clients << hunter = server.accept       # Wait for a client to connect
+hunter.puts(Time.now.ctime)  # Send the time to the client
+clients << prey = server.accept
+prey.puts(Time.now.ctime)  # Send the time to the client
 
-get '/' do
-  'EVASION'
-end
-
-class EvasionServer
-	def initialize
-		server = TCPServer.open(20000)  # Socket to listen on port 2000
-
-		clients = []
-		clients << hunter = server.accept       # Wait for a client to connect
-		hunter.puts(Time.now.ctime)  # Send the time to the client
-		clients << prey = server.accept
-		prey.puts(Time.now.ctime)  # Send the time to the client
-
-		clients.each{|c| c.puts "Closing"}
-		clients.each{|c| c.close}
-	end
-	
-	def run
-
-	end
-end
+clients.each{|c| c.puts "Closing"}
+clients.each{|c| c.close}
